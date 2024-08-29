599b639f4244160ae4e49961385872c79e96459c
Lucas Ribeiro Sampaio <160547725+lucas-software@users.noreply.github.com>
2024-08-29 18:31:33 +0000

    Tentativa 1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Insira dois valores para serem somados");
+		Scanner in = new Scanner(System.in);
+		double a = in.nextDouble();
+		double b = in.nextDouble();
+		System.out.println(somar(a,b));
+	}
+
+	public static double somar(double a, double b){
+		double s = a+b;
+		return s;
 	}
 
 }
